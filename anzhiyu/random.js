01455802a19e149004f8b4efa07160b2ed30d7c0
ub-w <1820234756@qq.com>
2025-12-06 16:26:58 +0800

    Site updated: 2025-12-06 16:26:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/28/hello-world/"];function toRandomPost(){
+var posts=["2025/11/28/hello-world/","2025/12/06/FPGA相关知识点0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
