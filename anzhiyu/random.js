07b57a31b9397faab14c16f226fbaa2495c18b09
ub-w <1820234756@qq.com>
2025-12-20 11:27:01 +0800

    Site updated: 2025-12-20 11:26:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/28/hello-world/","2025/12/06/FPGA相关知识点0/","2025/12/09/FPGA相关知识点1-generate语法/","2025/12/10/FPGA相关知识点2/","2025/12/11/FPGA相关知识点3---寄存器引入带来的延迟/"];function toRandomPost(){
+var posts=["2025/11/28/hello-world/","2025/12/06/FPGA相关知识点0/","2025/12/09/FPGA相关知识点1-generate语法/","2025/12/10/FPGA相关知识点2/","2025/12/11/FPGA相关知识点3---寄存器引入带来的延迟/","2025/12/19/FPGA相关知识点4/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
